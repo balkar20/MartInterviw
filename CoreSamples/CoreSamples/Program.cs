e5a2864ffea9a4d145bf29ed5ed2a0ca5ac87095
user <user@WIN-3VBHTKR11U0>
2019-04-09 21:33:45 +0300

    mnbn

---
+++ CoreSamples/CoreSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
