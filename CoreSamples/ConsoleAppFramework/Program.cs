eb20a4f91c8a0dd34cbf1d3fc1607e1f43a7ac6c
balkar20 <balkar20@mail.ru>
2019-04-10 17:37:13 +0300

    juhj

--- CoreSamples/ConsoleAppFramework/Program.cs
+++ CoreSamples/ConsoleAppFramework/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Standart1;
 
-namespace CoreSamples
+namespace ConsoleAppFramework
 {
     class Program
     {
---
+++ CoreSamples/Standart1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Standart1
+{
+    public class Class1
+    {
+        public void Method()
+        {
+            
+        }
+    }
+}
