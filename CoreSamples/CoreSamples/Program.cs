d2aac402290c4b5590a0871fceff7eaa2f99845e
user <user@WIN-3VBHTKR11U0>
2019-04-10 21:24:15 +0300

    yyy

--- CoreSamples/CoreSamples/Program.cs
+++ CoreSamples/CoreSamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary1;
 using Standart1;
 
 namespace CoreSamples
@@ -9,6 +10,8 @@ namespace CoreSamples
         {
             Class1 obj = new Class1();
             obj.Method();
+            FrameworkClass fc = new FrameworkClass();
+            fc.Meth();
         }
     }
 }
