a54d2a81329a5f5e5233a7ffc278afb34adfe2b3
user <user@WIN-3VBHTKR11U0>
2019-04-20 23:24:39 +0300

    kkk

---
+++ Bundling/Grunt/Gruntfile.js
@@ -0,0 +1,46 @@
+﻿module.exports = function (grunt) {
+	grunt.initConfig({
+		concat: {
+			build: {
+				src: ['wwwroot/js/*'],
+				dest: 'wwwroot/js/app.js'
+			}
+		},
+		uglify: {
+			build: {
+				files: {
+					'wwwroot/bundles/jquery.js': ['wwwroot/lib/jquery/dist/jquery.js'],
+					'wwwroot/bundles/jqueryval.js': ['wwwroot/lib/jquery-validation/dist/jquery.validate.js', 'lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js'],
+					'wwwroot/bundles/bootstrap.js': ['wwwroot/lib/bootstrap/dist/js/bootstrap.min.js'],
+					'wwwroot/bundles/app.js': ['wwwroot/js/app.js']
+				}
+			}
+		},
+		clean: ["wwwroot/js/app.js"],
+		cssmin: {
+			build: {
+				files: {
+					'wwwroot/bundles/main.css': ['wwwroot/lib/bootstrap/dist/css/bootstrap.css', 'wwwroot/css/site.css']
+				}
+			}
+		},
+		copy: {
+			build: {
+				expand: true,
+				cwd: 'wwwroot/lib/bootstrap/dist/fonts/',
+				src: '**',
+				dest: 'wwwroot/fonts/',
+				flatten: true,
+				filter: 'isFile'
+			},
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('build', ['clean', 'copy', 'concat', 'uglify', 'cssmin']);
+};
